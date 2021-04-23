a764578c2ade62391a638a3f2c6c808b5ac1b3f1
Utsav Gupta <utgupta27@hotmail.com>
2021-04-23 12:51:36 +0530

    Applied more conditions

--- rubbish code/dynamic programs/totalPathCount.py
+++ rubbish code/dynamic programs/totalPathCount.py
@@ -8,19 +8,38 @@ def memoization(func):
         return cache[key]
     return inner
 
-# List of the Routes That are Closed, We cannot go through that routes
-# closed routes are in the format [[row1,col1],[row2,col2],...]
-routeClosed = [[1,1],[2,2],[3,3]]
-
-# function to calculate the total no of possible paths form origin to opposite corner.
+# Find all the possible routes for one point to the end.
 @memoization
 def totalPathCount(row,column):
     if row < 0 : return 0
     if column < 0 :return 0
-    if [row,column] in routeClosed: return 0
     if row == 0 and column ==0 : return 1
     return totalPathCount(row-1,column) + totalPathCount(row,column-1)
 
+# List of the Routes That are Closed, We cannot go through that routes
+# closed routes are in the format [[row1,col1],[row2,col2],...]
+routeClosed = [[1,1]]
+
+# function to calculate the total no of possible paths form origin to
+# opposite corner when some routes are closed
+@memoization
+def totalPathCountWithClosedRoutes(row,column):
+    if row < 0 : return 0
+    if column < 0 :return 0
+    if [row,column] in routeClosed: return 0
+    if row == 0 and column ==0 : return 1
+    return totalPathCountWithClosedRoutes(row-1,column) + totalPathCountWithClosedRoutes(row,column-1)
+
+# if we are allowed to move diagonally
+@memoization
+def totalPathCountDiagonally(row,column):
+    if row < 0 : return 0
+    if column < 0 :return 0
+    # To eliminate Closed Route paths uncomment the line just below
+    if [row,column] in routeClosed: return 0
+    if row == 0 and column ==0 : return 1
+    return totalPathCountDiagonally(row-1,column) + totalPathCountDiagonally(row,column-1) + totalPathCountDiagonally(row-1,column-1)
+
 # Diver Function for testing Purpose
 if __name__ == '__main__':
-    print(totalPathCount(4,4))
+    print(totalPathCountDiagonally(2,2))
