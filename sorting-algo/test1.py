4b21490d4ad52df2048f31d1bea965b9f2a0a77e
utgupta27 <utgupta27@gmail.com>
2021-07-21 12:08:15 +0530

    linsearch

--- sorting-algo/test1.py
+++ sorting-algo/test1.py
@@ -1,27 +1,13 @@
-# Implementation of Quick Sort Algorithm
+def linearSearch(arr,key):
+    res = -1
+    for i in range(len(arr)):
+        if key == arr[i]:
+            res = i
+            return res
+    return res
 
-def quickSort(start, end, arr):
-    if start < end:
-        p = quickPartition(start, end, arr)
-        quickSort(start, p-1, arr)
-        quickSort(p+1, end, arr)
-
-
-def quickPartition(start, end, arr):
-    pivotIndex = start
-    pivot = arr[start]
-    while start < end:
-        while start < len(arr) and arr[start] <= pivot:
-            start += 1
-        while arr[end] > pivot:
-            end -= 1
-        if start < end:
-            arr[start],arr[end] = arr[end], arr[start]
-    arr[pivotIndex], arr[end] = arr[end], arr[pivotIndex]
-    return end
 
 
 if __name__ == "__main__":
-    lst0 = [9,7,5,3,1,8,6,4,3,0,34,65,343,79,90,85,21,79,57,49,61,62,63,88,77,66,55,44,33,22,11,100,99]
-    quickSort(0,len(lst0)-1,lst0)
-    print(lst0)
+    lst = [0,1,2,3,4,5,6,6,6,6,7,8,9]
+    print(linearSearch(lst,6))
